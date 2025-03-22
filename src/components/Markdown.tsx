708a05a60f36b13d9f32cc6d8649a01ed24e54f5
John Paul R <jp@jpcode.dev>
2025-03-22 19:04:20 -0400

    open external links from markdown in browser

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -4,24 +4,31 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
+import { autoLinkMd } from "react-markdown-autolink";
 
 // https://dev.azure.com/ORGANIZATION/PROJECT/_git/REPOSITORY/pullrequest/32627
 const PR_REGEX =
     /^https:?\/\/dev.azure.com\/.+?\/.+?\/_git\/.+\/pullrequest\/(\d+)/;
 
 export function Markdown({ markdown }: { markdown: string }) {
+    markdown = autoLinkMd(markdown);
     return (
         <ReactMarkdown
             children={markdown}
             rehypePlugins={[rehypeSanitize, rehypeRaw]}
             components={{
                 a(props) {
-                    const { children, className, href, ...rest } = props;
+                    const { children, className, href, node, ...rest } = props;
                     const match = href ? PR_REGEX.exec(href) : "";
-
+                    const linkTo = match?.[1]
+                        ? `/pr/${match?.[1]}`
+                        : href ?? "/";
                     return (
                         <Link
-                            to={match?.[1] ? `/pr/${match?.[1]}` : href ?? "/"}
+                            target={
+                                linkTo.startsWith("http") ? "_blank" : undefined
+                            }
+                            to={linkTo}
                             className={className}
                             {...rest}
                         >
